a055e63f881687b18a776472186d5bd4eda2a2d1
DPOLE <DPOLE@DPOLEui-MacBook-Pro.local>
2019-06-05 09:59:57 +0900

    Update sketch.js

--- example/sketch.js
+++ example/sketch.js
@@ -1,9 +1,10 @@
+let x = 0;
 function setup() {
   title = createElement('h2', "<a href='/PoeticCodeForNature'> HOME : </a> 작품 제목");
   title.position(20, 0);
 
   canvas = createCanvas(300, 300);
-  canvas.position(20, 60);
+  canvas.position(x, 60);
   canvas.class("artwork");
 
   description = "\
@@ -21,4 +22,6 @@ function draw() {
   background(0);
   fill(255);
   ellipse(width/2, height/2, 50);
+    canvas.position(x, 60);
+    x++;
 }
